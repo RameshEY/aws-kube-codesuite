7dc403b9e496f3fb2c691c384ae7a8c13f3ecb85
Sam Myers <sam@portworx.com>
2018-01-17 11:09:16 -0800

    Convert sample app to work with a MongoDB backend
    Requires unauthenticated MongoDB instance at hostname `mongo` on port `27017`
    
    Signed-off-by: Sam Myers <sam@portworx.com>

--- sample-app/hello.py
+++ sample-app/hello.py
@@ -1,12 +1,22 @@
 from flask import Flask, jsonify
-import boto3
-from werkzeug.exceptions import NotFound
+from pymongo import MongoClient
+import time
 
 app = Flask(__name__)
+mongo = MongoClient('mongo', 27017)
+db = mongo.sample_database
+collection = db.widgets
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello, World!'
+    return 'Hello!\nThere are {} items in the database'.format(collection.count())
+
+@app.route('/add', methods=['POST'])
+def add():
+    collection.insert_one({
+        'timestamp': int(time.time())
+    })
+    return 'You have added an item to the database!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
