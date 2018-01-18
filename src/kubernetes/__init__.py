e7ac0366404d9aaf9b74e49e00598f4ea69a73bd
Sam Myers <sam@portworx.com>
2018-01-18 15:48:13 -0800

    Update Kubernetes to version 4
    Required to make use of `exec` commands
    
    Signed-off-by: Sam Myers <sam@portworx.com>

--- src/kubernetes/__init__.py
+++ src/kubernetes/__init__.py
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__project__ = 'kubernetes'
+# The version is auto-updated. Please do not edit.
+__version__ = "4.0.0"
+
 import kubernetes.client
 import kubernetes.config
 import kubernetes.watch
+import kubernetes.stream
