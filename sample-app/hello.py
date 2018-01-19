c0aa7ecd5ed1135c4e9c0cd146b3e968f5a728e0
Sam Myers <sam@portworx.com>
2018-01-19 13:23:45 -0800

    Handle MongoDB connection errors
    Signed-off-by: Sam Myers <sam@portworx.com>

--- sample-app/hello.py
+++ sample-app/hello.py
@@ -1,22 +1,34 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 import time
 
 app = Flask(__name__)
-mongo = MongoClient('mongo', 27017)
+mongo = MongoClient('mongo', 27017,
+                    connectTimeoutMS=1000, socketTimeoutMS=1000, serverSelectionTimeoutMS=1000,
+                    appname='demo-app')
 db = mongo.sample_database
 collection = db.widgets
 
+
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello!\nThere are {} items in the database'.format(collection.count())
+    try:
+        count = collection.count()
+        return 'Hello!\nThere are {} items in the database\n'.format(count)
+    except ServerSelectionTimeoutError:
+        return "Can't connect to MongoDB\n"
 
 @app.route('/add', methods=['POST'])
 def add():
-    collection.insert_one({
-        'timestamp': int(time.time())
-    })
-    return 'You have added an item to the database!'
+    try:
+        collection.insert_one({
+            'timestamp': int(time.time())
+        })
+    except ServerSelectionTimeoutError:
+        return "Can't connect to MongoDB\n"
+    return 'You have added an item to the database!\n'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
